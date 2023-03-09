341f2e239d9df86b9a699e24c9bdfe43e8e5949a
MDOR <mikhailsinghfc@gmail.com>
2023-03-09 16:34:14 +0200

    Fix file path bug
    - If your filename had a space in it, it would cause
    an error to be thrown. Possibly caused by the command
    and subprocess methods

--- BackgroundMusicRemover.py
+++ BackgroundMusicRemover.py
@@ -23,10 +23,9 @@ for file in files:
     #Use Spleeter to cut out the music from the video
     separator = Separator('spleeter:5stems', multiprocess=False)
     separator.separate_to_file("Input/"+file.name, '')
-
     #Use FFMpeg to replace the audio track of the video with the output vocal track and write it to the output dir
-    command = 'ffmpeg -y -i input/'+file.name+' -i '+ file_name +'/vocals.wav -c:v copy -map 0:v:0 -map 1:a:0 output/'+file_name+'_output.mp4'
-    command = shlex.split(command)
+    command = 'ffmpeg -y -i "input/'+file.name+'" -i "'+ file_name +'/vocals.wav" -c:v copy -map 0:v:0 -map 1:a:0 "output/'+file_name+'_output.mp4"'
+
     subprocess.call(command)
     shutil.rmtree(file_name)
     print(file_name+" is completed")
